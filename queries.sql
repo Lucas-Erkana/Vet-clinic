167ce3b83bc849ba3c3ccda4d735c5ef91047891
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-02 16:27:56 +0200

    Delete all records and rollback

--- queries.sql
+++ queries.sql
@@ -19,5 +19,17 @@ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 
-
 SELECT * FROM animals
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals
+
+ROLLBACK;
+SELECT * FROM animals
+
+
+
+
+
+
