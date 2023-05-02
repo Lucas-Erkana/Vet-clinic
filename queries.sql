3982c1b7c3bdf7852f7b8ca0da6f70cef4d43032
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-02 16:39:54 +0200

    Delete, Save, Rollback and Commit

--- queries.sql
+++ queries.sql
@@ -28,6 +28,27 @@ SELECT * FROM animals
 ROLLBACK;
 SELECT * FROM animals
 
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT my_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO my_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+
+
+
+
+
+
+
 
 
 
