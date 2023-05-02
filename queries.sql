b101d0f56cd28ecac899c0476d4411cebf4683ab
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-02 16:21:25 +0200

    Update species to either gigimon or pokemon

--- queries.sql
+++ queries.sql
@@ -13,3 +13,10 @@ BEGIN;
 UPDATE animals SET species = 'unspecified';
 
 ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+SELECT * FROM animals
