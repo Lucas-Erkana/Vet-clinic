887a9bcaec81082f84fa3da5d9751394f75376f6
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-02 16:15:34 +0200

    Update species and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,8 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+
+ROLLBACK;
