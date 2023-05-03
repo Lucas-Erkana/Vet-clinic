160f21e9e837cbec5a5471450e22551ef6fc9af0
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-03 15:29:35 +0200

    Modify inserted animals so it includes the species_id

--- data.sql
+++ data.sql
@@ -38,4 +38,11 @@ INSERT INTO species (name) VALUES
 ('Pokemon'),
 ('Digimon');
 
+UPDATE animals
+SET species_id = 
+  CASE 
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+  END;
+
 
