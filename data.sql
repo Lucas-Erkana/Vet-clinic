cbb80946cf2ce8a706385ba94e03d1d3692276e3
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-03 15:23:07 +0200

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -26,5 +26,12 @@ VALUES ('Blossom', '1998-10-13', 3, true, 17);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-05-14', 4, true, 22);
 
+INSERT INTO owners (full_name, age) VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
 
 
