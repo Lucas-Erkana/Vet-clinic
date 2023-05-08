51620d116fddbba26d3ed5e7e773570a44cce83f
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-08 17:15:15 +0300

    Update queries.sql file

--- queries.sql
+++ queries.sql
@@ -187,12 +187,6 @@ ORDER BY num_visits DESC
 LIMIT 1;
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-
-
--- BEFORE
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-
---
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
